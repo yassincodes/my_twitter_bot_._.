a8778faabf0b23e8b5da4ee66c52cca469e6a283
yassin <meddyassin@hotmail.com>
2022-03-18 02:40:39 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+// I took a 3 hour course on typescript 
+// and built 2 small websites with it 
+// should I add it to my resume?
+
+// oh I also solved 5kyu question on codewars with it
+
+// I was just playing with my locations on twitter
+// and I found something really weird
+// you can basically change your location to any square color 
+// but only blue!!!! why is that @twitter
+// I just wanted to add typescript to my list of tech I know here
+// then twitter told me I can't use the emoji lol
+
+// whenever a user follows me, my twitter photo will change to his photo 
+// in no time .. go and try it out 
 const dotenv = require("dotenv")
 const Twitter = require("twitter")
 
@@ -12,7 +27,7 @@ const client = new Twitter({
 
 async function main() {
     const params = {
-      location: "hi"
+      description: "css/js/react/firebase"
     };
     await client.post("account/update_profile", params);
     console.log("🎉 Success! Updated Twitter bio/location and website");
